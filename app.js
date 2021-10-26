bb6b07a4e042b8ba735cf9a0a46cbf2fd3daeade
Shashank Singh <62293640+ssrajputtheboss@users.noreply.github.com>
2021-10-26 18:17:42 +0530

    add: models,socket.io; remove: express

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-const express = require('express')
-const cors = require('cors')
-const { PORT } = require('./config/config')
-const AuthController = require('./controller/AuthController')
-const app = express()
-
-
-app.use(cors())
-app.use(express.json())
-AuthController.login()
-
-
-app.listen(PORT,console.log(`${PORT}`))
+console.log('hello node')
--- models/roomschema.js
+++ models/roomschema.js
@@ -1,3 +1,16 @@
-/*
-room schema for live rooms
-*/
+const {Schema , model} = require('mongoose')
+
+const HubSchema = new Schema({
+    adminId : Number,
+    hubName : String,
+    hubID : String,
+    hubTopic : String,
+    hubCode: {
+        type: String,
+        default: ''
+    },
+    isPublic : Boolean,
+    users : []
+})
+
+module.exports = model('hubshcema',HubSchema)
